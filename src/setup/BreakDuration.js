872bff6a17fac984eb3b7a9cfaff2561f6cdd5ae
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-16 01:55:27 -0500

    broken down into 5 additional components

---
+++ src/setup/BreakDuration.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function BreakDuration() {
+  return (
+    <div className="col">
+      <div className="float-right">
+        <div className="input-group input-group-lg mb-2">
+          <span className="input-group-text" data-testid="duration-break">
+            {/* TODO: Update this text to display the current break session duration */}
+            Break Duration: 05:00
+          </span>
+          <div className="input-group-append">
+            {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-testid="decrease-break"
+            >
+              <span className="oi oi-minus" />
+            </button>
+            {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-testid="increase-break"
+            >
+              <span className="oi oi-plus" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BreakDuration;
---
+++ src/setup/Setup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FocusDuration from "./FocusDuration";
+import BreakDuration from "./BreakDuration";
+
+function Setup() {
+  return (
+    <div className="row">
+      <FocusDuration />
+      <BreakDuration />
+    </div>
+  );
+}
+
+export default Setup;
